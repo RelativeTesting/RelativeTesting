8a23d5f6ab4af0bebea248da4358507f13f50223
Enis Mert Kuzu <emertkuzu35@gmail.com>
2023-04-07 12:05:26 +0200

    add input detection

--- generator/func_generator.py
+++ generator/func_generator.py
@@ -1,6 +1,42 @@
 import os
 import re
 import datetime
+import ast
+
+class InputVisitor(ast.NodeVisitor):
+    def __init__(self):
+        self.user_inputs = {}
+
+    def visit_Assign(self, node):
+        for child in ast.walk(node.value):
+            if isinstance(child, ast.Call):
+                if isinstance(child.func, ast.Name):
+                    if child.func.id == 'input':
+                        for target in node.targets:
+                            if isinstance(target, ast.Name):
+                                print(target.id)
+                                if(str(target.id)  not in self.user_inputs):
+                                   self.user_inputs[target.id]=str
+                    elif child.func.id == 'int':
+                        for target in node.targets:
+                            if isinstance(target, ast.Name):
+                                self.user_inputs[target.id] = int
+                                break
+                    elif child.func.id == 'float':
+                        for target in node.targets:
+                            if isinstance(target, ast.Name):
+                                self.user_inputs[target.id] = float
+                                break
+        self.generic_visit(node)
+
+
+def detect_user_inputs(code):
+    tree = ast.parse(code)
+    visitor = InputVisitor()
+    visitor.visit(tree)
+    return visitor.user_inputs
+
+
 
 # current date and time info
 now = datetime.datetime.now()
@@ -26,35 +62,32 @@ for file_name in file_list:
     with open(file_path, 'r') as file:
         contents = file.read()
         lines = contents.splitlines()
-
-    tabbed_lines = []
-    tabbed_lines.append("def function(")
-    for line in lines:
+    user_inputs = detect_user_inputs(contents)
+    # convert input statements to function parameters
+    new_params = []
+    for param_name, param_type in user_inputs.items():
+        new_params.append(f'{param_name}: {param_type.__name__}')
+        
+    # remove input statements and create function signature
+    function_lines = []
+    for i, line in enumerate(lines):
+        pattern = r'input\s*\((.*?)\)(?![^(]*\))'
+        if re.search(pattern, line):
+            # skip input lines
+            continue
+        # indent line
         match = re.match(r'^(\s*)(.*)$', line)
         indentation, content = match.group(1), match.group(2)
         tabbed_line = '\t' + indentation + content
-        tabbed_lines.append(tabbed_line)
+        function_lines.append(tabbed_line)
 
-    # pattern of an input statement
-    pattern = r'input\s*\((.*?)\)(?![^(]*\))'
+    # add function signature
+    function_lines.insert(0, 'def my_function(' + ', '.join(new_params) + '):')
 
-    count = 1
-    for i, line in enumerate(tabbed_lines):
+    # combine lines and return function code
+    function_code = '\n'.join(function_lines)
 
-        if re.search(pattern, line):
-            parameter_name = f'param{count}' 
-            if count == 1:
-                tabbed_lines[0] = tabbed_lines[0] + parameter_name
-            else:
-                tabbed_lines[0] = tabbed_lines[0] + ", " + parameter_name
-            tabbed_lines[i] = re.sub(pattern, parameter_name + ")", line)
-            count += 1
-
-    # TODO parentheses handling in the input string
-
-    tabbed_lines[0] = tabbed_lines[0] + "):"
-    contents = '\n'.join(tabbed_lines)
 
     output_file_path = output_directory + "/" + file_name[:-3] + "_" + date_time + ".py"
     with open(output_file_path, 'w') as modified_file:
-        modified_file.write(contents)
+        modified_file.write(function_code)

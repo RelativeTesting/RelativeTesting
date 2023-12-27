a2eb7488785e16c8c0f6ee614b1f07e158997836
Enis Mert Kuzu <emertkuzu35@gmail.com>
2023-12-27 17:36:32 +0300

    update preconstraints

--- test/single/test_str.py
+++ test/single/test_str.py
@@ -30,13 +30,6 @@ def test_str(param1, param2):
     #     ethernet_delay_seconds, '.4f'), 'second(s)')
 
 
-@types(x="int", y = "int")
-@symbolic(x="@(x > 10 and x < 20) and ((x == 11) or (x == 18))")
-#@symbolic(x="@(x > y and y==6)")
-def test_str(x, y):
-    if x > 15:
-        return 1 
-    return 2
 
 # # @symbolic(x = "@(x != 2) and (y > 2) ")
 # def test_str(x,y):

5f2381f473cbacc973d9dbec9a0b8204a0dc6e13
Enis Mert Kuzu <emertkuzu35@gmail.com>
2024-01-04 15:29:04 +0300

    add some test cases

---
+++ AST/test.py
@@ -0,0 +1,27 @@
+#testcase1
+for i in range(34):
+    print(i)
+
+#testcase2
+i=0  
+while i<10:
+    print(i)
+    i+=1
+
+#testcase3
+for i in range(10):
+    x=int(input("Enter a number: "))
+
+#testcase4
+def test():
+    print("Hello World")
+
+#testcase5
+def test():
+    for i in range(10):
+        print(i)
+
+#testcase6
+def test():
+    whilefor i in range(10):
+        
